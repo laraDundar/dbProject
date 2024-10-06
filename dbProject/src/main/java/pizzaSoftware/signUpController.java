65de61d0d6c8f77488711d9ea76ac972a0aedbb7
laraDundar <166695216+laraDundar@users.noreply.github.com>
2024-10-06 13:57:59 +0200

    Added the connection between registration code and gui.

--- dbProject/src/main/java/pizzaSoftware/signUpController.java
+++ dbProject/src/main/java/pizzaSoftware/signUpController.java
@@ -1,5 +1,79 @@
 package pizzaSoftware;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import java.time.LocalDate;
+
 public class signUpController {
+
+    @FXML
+    private TextField newAccountUsernameTextField;
     
+    @FXML
+    private TextField newAccountPasswordTextField;
+
+    @FXML
+    private TextField newAccountEmailTextField;
+    
+    @FXML
+    private TextField newAccountNameTextField;
+
+    @FXML
+    private TextField newAccountSurnameTextField;
+
+    @FXML
+    private DatePicker newAccountBirthSelector;
+
+    @FXML
+    private TextField newAccountPhoneTextField;
+
+    @FXML
+    private ComboBox<String> newAccountGenderComboBox;
+
+    @FXML
+    private TextField newAccountAddressTextField;
+
+    @FXML
+    private TextField newAccountZipTextField;
+
+    @FXML
+    private Button signUpButton;
+
+    private LoginManager loginManager;
+
+    public signUpController() {
+        this.loginManager = new LoginManager();
+    }
+
+    @FXML
+    public void initialize() {
+        newAccountGenderComboBox.getItems().addAll("Male", "Female");
+    }
+
+    @FXML
+    private void signUpAction(ActionEvent event) {
+        // Get data from the fields
+        String username = newAccountUsernameTextField.getText();
+        String password = newAccountPasswordTextField.getText();
+        String email = newAccountEmailTextField.getText();
+        String firstName = newAccountNameTextField.getText();
+        String lastName = newAccountSurnameTextField.getText();
+        LocalDate birthdate = newAccountBirthSelector.getValue();
+        String phoneNumber = newAccountPhoneTextField.getText();
+        String gender = newAccountGenderComboBox.getValue();
+        String address = newAccountAddressTextField.getText();
+        String zipCode = newAccountZipTextField.getText();
+
+        Customer newCustomer = new Customer(email, username, firstName, lastName, gender, birthdate, phoneNumber, address, zipCode);
+
+        loginManager.registerCustomer(newCustomer, password);
+        
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Registration Success!");
+        alert.setHeaderText(null);
+        alert.setContentText("You are registered successfully!");
+        alert.showAndWait();
+    }
+
 }
