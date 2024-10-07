f08c86753aa8ede7fa70e4abfb8a87f89587ee67
laraDundar <166695216+laraDundar@users.noreply.github.com>
2024-10-07 21:08:10 +0200

    basics  design of earnings report

---
+++ dbProject/src/main/java/pizzaSoftware/reportController.java
@@ -0,0 +1,44 @@
+package pizzaSoftware;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class reportController {
+
+    @FXML
+    private TableView<?> reportTable;
+
+    @FXML
+    private Label filtersLabel;
+
+    @FXML
+    private ComboBox<?> ageComboBox;
+
+    @FXML
+    private Label reportTitleLabel;
+
+    @FXML
+    private ComboBox<?> regionComboBox;
+
+    @FXML
+    private ComboBox<?> genderComboBox;
+
+    @FXML
+    void regionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void genderAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ageAction(ActionEvent event) {
+
+    }
+    
+}
